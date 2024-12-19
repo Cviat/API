281441eccc934ac9c02548b4337b6cba70b3a247
Cviat <svt-2000@mail.ru>
2024-12-19 23:27:27 +0300

    Добавить .gitattributes для нормализации окончаний строк

---
+++ basic/migrations/m241219_185809_create_bid.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241219_185809_create_bid
+ */
+class m241219_185809_create_bid extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('requests', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'email' => $this->string()->notNull(),
+            'status' => "ENUM('Active', 'Resolved') NOT NULL DEFAULT 'Active'",
+            'message' => $this->text()->notNull(),
+            'comment' => $this->text()->null(),
+            'created_at' => $this->timestamp()->notNull(),
+            'updated_at' => $this->timestamp()->null(),
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('requests');
+    }
+
+
+}
